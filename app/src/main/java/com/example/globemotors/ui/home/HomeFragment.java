8e93aea4d3fe1dde1f122dcfc64bfb1e6e850efc
arshedgithub <arshedahmedmail@gmail.com>
2024-08-28 03:38:00 +0530

    adding list view

--- app/src/main/java/com/example/globemotors/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/globemotors/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,9 +14,16 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.globemotors.databinding.FragmentHomeBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> itemList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +35,17 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        listView = binding.productList;
+        itemList = new ArrayList<>();
+        itemList.add("Product Item 1");
+        itemList.add("Product Item 2");
+        itemList.add("Product Item 3");
+        itemList.add("Product Item 4");
+
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, itemList);
+        listView.setAdapter(adapter);
+
         return root;
     }
 
